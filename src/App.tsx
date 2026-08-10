0c4418f635d841ce78bfcf5e964f5921a360fe45
lilkasalananshortie <manalilimhakujose@gmail.com>
2026-08-11 00:59:54 +0800

    style(ui): redesign surfaces with bundled Poppins

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { NotificationProvider } from "./stores/notifications";
 import { LiveActivityProvider } from "./stores/liveActivity";
 import { FocusProvider } from "./stores/focus";
 import { TimerProvider } from "./stores/timer";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 import "./App.css";
 
 export default function App() {
